17ec54e431b8969bff52be9ac9fa18e2a7108013
Kenneth Reed <kenakioreed@gmail.com>
2024-03-17 17:02:10 -0400

    foo

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() {
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
         draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
         
-        draw_texture_ex(&tileset,50.0,50.0,WHITE,DrawTextureParams{dest_size:None,source:Some(Rect{x:0.0,y:0.0,w:100.0,h:150.0}),rotation:0.0,flip_x:false,flip_y:false,pivot:None});
+        draw_texture_ex(&tileset,50.0,50.0,WHITE,DrawTextureParams{dest_size:None,source:Some(Rect{x:0.0,y:0.0,w:88.0,h:124.0}),rotation:0.0,flip_x:false,flip_y:false,pivot:None});
         draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
 
         next_frame().await
